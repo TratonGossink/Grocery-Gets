b1ad53cc57f7fc7e8bbf5de35fc684976bb5a9a2
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-12-22 05:39:42 -0600

    Initial Commit

---
+++ Grocery Gets/Grocery_GetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_GetsApp.swift
+//  Grocery Gets
+//
+//  Created by Traton Gossink on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_GetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
