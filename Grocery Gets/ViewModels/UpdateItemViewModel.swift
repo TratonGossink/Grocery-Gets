0e523810596360665ccb2078b4df1b7baf9da914
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-12-27 04:47:07 -0600

    - Add launch screen call to root view - Remove unused categoryItem from AddItem - Alter date format in AddItemCell - Add UpdateViewModel

---
+++ Grocery Gets/ViewModels/UpdateItemViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  UpdateItemViewModel.swift
+//  Grocery Gets
+//
+//  Created by Traton Gossink on 12/26/23.
+//
+
+import SwiftUI
+
+struct UpdateItemViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateItemViewModel()
+}
