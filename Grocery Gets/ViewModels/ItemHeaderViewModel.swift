4c56b8af557aef7eee4fa155dd4a4cffd0bf200d
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2024-01-01 11:27:05 -0600

    - Add collapsing and extanding list view - Integrate Launch screen - Update AddItemSheet - Add ItemHeaderViewModel - Add UpdateItemViewModel

---
+++ Grocery Gets/ViewModels/ItemHeaderViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ItemHeaderViewModel.swift
+//  Grocery Gets
+//
+//  Created by Traton Gossink on 12/27/23.
+//
+
+import SwiftUI
+
+struct ItemHeaderViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemHeaderViewModel()
+}
