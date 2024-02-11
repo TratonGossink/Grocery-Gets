a0ccb79b6406c29db839e739c7ebc71fa4c4ed9f
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2024-02-11 11:22:39 -0600

    Add - RealmSwift
    Add - RootView
    
    Add - ListViewModel

---
+++ Grocery Gets/ViewModels/ListViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ListViewModel.swift
+//  Grocery Gets
+//
+//  Created by Traton Gossink on 2/11/24.
+//
+
+import SwiftUI
+
+struct ListViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListViewModel()
+}
